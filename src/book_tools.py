144698825dea2457c3049321391f8635fbb2f1f2
Dan Tkachov <danieltkachov67@gmail.com>
2026-06-15 19:48:59 -0400

    Initial commit, setting up ideas

---
+++ src/book_tools.py
@@ -0,0 +1,5 @@
+# Tool 1: List books
+# Tool 2: Create Directory
+# Tool 3: Book renaming
+# Tool 4: Move book from one dir to another
+# Tool 5: Currently unorganized books
---
+++ src/state.py
@@ -0,0 +1,8 @@
+# Store unorganized books into a  json list
+# when a book is categorized, remove it from the json
+# only books in a specific folder called "book dump" are categorized
+# files can be in book dump as well
+# pdf, epub, and other book formats only
+
+# also have a category master list; books should try to go into an existing
+# category if it makes sense.
